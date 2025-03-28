d6e4e4e02b4f5bacc1d88f704a868cf21f7b1931
Sushant_Nair <94003837+zealsnair@users.noreply.github.com>
2025-03-28 11:20:37 +0530

    Update Minimum-Path-Sum.cpp

--- Minimum-Path-Sum.cpp
+++ Minimum-Path-Sum.cpp
@@ -1,99 +1,89 @@
-//* https://www.codingninjas.com/codestudio/problems/minimum-path-sum_985349?leftPanelTab=0
-//* https://youtu.be/_rgTlyky1uQ
+#include <bits/stdc++.h>
+using namespace std;
 
-#include <bits/stdc++.h> 
-int rec(int row, int col, vector<vector<int>>& grid, vector<vector<int>>& dp){
-    if(row == 0 && col == 0){
-        return grid[row][col];
-    }
-    if(row < 0 || col < 0){
-        return 1e9; // or INT_MAX;
-    }
+const int INF = 1e9;
 
-    if(dp[row][col] != -1){
-        return dp[row][col];
-    }
+// **🔹 Memoization Approach (Top-Down DP)**
+int f(int row, int col, vector<vector<int>>& grid, vector<vector<int>>& dp) {
+    if (row < 0 || col < 0) return INF; // Out of bounds
+    if (row == 0 && col == 0) return grid[row][col]; // Base case: reached start
+    
+    if (dp[row][col] != -1) return dp[row][col];
 
-    int up = grid[row][col] + rec(row-1, col, grid, dp);
-    int left = grid[row][col] + rec(row, col-1, grid, dp);
+    int up = grid[row][col] + f(row - 1, col, grid, dp);
+    int left = grid[row][col] + f(row, col - 1, grid, dp);
 
     return dp[row][col] = min(up, left);
 }
-int minSumPath(vector<vector<int>> &grid) {
-    // Write your code here.
 
-    int m = grid.size();
-    int n = grid[0].size();
+int minSumPathMemo(vector<vector<int>>& grid) {
+    int m = grid.size(), n = grid[0].size();
+    vector<vector<int>> dp(m, vector<int>(n, -1));
+    return f(m - 1, n - 1, grid, dp);
+}
 
-    //* Recursion-> TLE
-    // return rec(m-1, n-1, grid);
+// **🔹 Tabulation Approach (Bottom-Up DP)**
+int minSumPathTab(vector<vector<int>>& grid) {
+    int m = grid.size(), n = grid[0].size();
+    vector<vector<int>> dp(m, vector<int>(n, 0));
 
-    //* DP Memoization->
-    // vector<vector<int>> dp(m, vector<int>(n,-1));
-    // return rec(m-1, n-1, grid, dp);
-
-    //* DP Tabulation->
-    // int dp[m][n];
-    // for(int i = 0; i < m; ++i){
-    //     for(int j = 0; j < n; ++j){
-    //         if(i == 0 && j == 0){
-    //             dp[i][j] = grid[i][j];
-    //         }
-    //         else{
-    //             int up , left;
-    //             up = left = grid[i][j];
-                
-    //             if(i > 0){
-    //                 up += dp[i-1][j];
-    //             }
-    //             else{
-    //                 up = 1e9;
-    //             }
-
-    //             if(j > 0){
-    //                 left += dp[i][j-1];
-    //             }
-    //             else{
-    //                 left = 1e9;
-    //             }
-
-    //             dp[i][j] = min(up, left);
-    //         }
-    //     }
-    // }
-    // return dp[m-1][n-1];
-
-
-    //* DP (Tabulation + Space Optimisation)->
-    vector<int> prev(n, 0);
-    for(int i = 0; i < m; ++i){
-        vector<int> curr(n, 0);
-        for(int j = 0; j < n; ++j){
-            if(i == 0 && j == 0){
-                curr[j] = grid[i][j];
+    for (int i = 0; i < m; ++i) {
+        for (int j = 0; j < n; ++j) {
+            if (i == 0 && j == 0) dp[i][j] = grid[i][j]; // Start point
+            else {
+                int up = (i > 0) ? dp[i - 1][j] : INF;
+                int left = (j > 0) ? dp[i][j - 1] : INF;
+                dp[i][j] = grid[i][j] + min(up, left);
             }
-            else{
-                int up , left;
-                up = left = grid[i][j];
-                
-                if(i > 0){
-                    up += prev[j];
-                }
-                else{
-                    up = 1e9;
-                }
+        }
+    }
+    return dp[m - 1][n - 1];
+}
 
-                if(j > 0){
-                    left += curr[j-1];
-                }
-                else{
-                    left = 1e9;
-                }
+// **🔹 Space-Optimized Approach**
+int minSumPathSpaceOptimized(vector<vector<int>>& grid) {
+    int m = grid.size(), n = grid[0].size();
+    vector<int> prev(n, INF);
 
-                curr[j] = min(up, left);
+    for (int i = 0; i < m; ++i) {
+        vector<int> curr(n, INF);
+        for (int j = 0; j < n; ++j) {
+            if (i == 0 && j == 0) curr[j] = grid[i][j]; // Start point
+            else {
+                int up = (i > 0) ? prev[j] : INF;
+                int left = (j > 0) ? curr[j - 1] : INF;
+                curr[j] = grid[i][j] + min(up, left);
             }
         }
         prev = curr;
     }
-    return prev[n-1];
+    return prev[n - 1];
+}
+
+// **🔹 Main function to take user input**
+int main() {
+    int m, n;
+    cout << "Enter the number of rows: ";
+    cin >> m;
+    cout << "Enter the number of columns: ";
+    cin >> n;
+
+    if (m <= 0 || n <= 0) {
+        cout << "Invalid grid size." << endl;
+        return 0;
+    }
+
+    vector<vector<int>> grid(m, vector<int>(n));
+    cout << "Enter the grid values row by row:" << endl;
+    for (int i = 0; i < m; ++i) {
+        for (int j = 0; j < n; ++j) {
+            cin >> grid[i][j];
+        }
+    }
+
+    cout << "\nMemoization Approach: " << minSumPathMemo(grid) << endl;
+    cout << "Tabulation Approach: " << minSumPathTab(grid) << endl;
+    cout << "Space Optimized Approach: " << minSumPathSpaceOptimized(grid) << endl;
+
+    return 0;
 }
